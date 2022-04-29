dc9e916324dafbf8c606222024ef1403680289c7
ea.kichaev <evgeniy.kichaev@kodep.ru>
2022-04-29 14:29:51 +0300

    add postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env' }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.PG_CONNECTION_URL,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+      logging: true,
+    }),
   ],
 })
 export class AppModule {}
