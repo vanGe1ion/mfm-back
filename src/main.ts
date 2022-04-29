87fabaeab88cce35952f227aa7f6b5805f191528
ea.kichaev <evgeniy.kichaev@kodep.ru>
2022-04-29 14:23:15 +0300

    clear project add @nestjs/config add env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SERVER_PORT } from './config';
+import 'reflect-metadata';
 
-async function bootstrap() {
+async function start() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(SERVER_PORT, () =>
+    console.log(`Server started on port ${SERVER_PORT}`),
+  );
 }
-bootstrap();
+
+start();
