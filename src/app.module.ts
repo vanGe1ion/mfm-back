a857c4d318306e9d7069f690a52687de910c5279
ea.kichaev <evgeniy.kichaev@kodep.ru>
2022-04-29 14:47:55 +0300

    edit Typeorm module config

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'postgres',
       url: process.env.PG_CONNECTION_URL,
       entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
       autoLoadEntities: true,
+      synchronize: false,
       logging: true,
+      migrations: ['dist/migrations/*.js'],
+      cli: {
+        migrationsDir: 'src/migrations',
+      },
     }),
   ],
 })
