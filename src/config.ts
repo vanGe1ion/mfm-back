2d0f739127d8de682bde63cac95d803d704db0d7
ea.kichaev <kichaev1@mail.ru>
2022-05-17 10:52:11 +0300

    rename env backend port

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_SERVER_PORT = 5000;
+export const DEFAULT_BACKEND_PORT = 5000;
 
 export const TMDB_SEARCH_PAGE_LIMIT = 5;
 export const TMDB_LANGUAGE = 'en-EN';
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DEFAULT_SERVER_PORT } from './config';
+import { DEFAULT_BACKEND_PORT } from './config';
 import 'reflect-metadata';
 import { ConfigService } from '@nestjs/config';
 
 async function start() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('SERVER_PORT') || DEFAULT_SERVER_PORT;
+  const port = configService.get<number>('BACKEND_PORT') || DEFAULT_BACKEND_PORT;
   await app.listen(port, () => console.log(`Server started on port ${port}`));
 }
 
